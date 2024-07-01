f74dc32b3bab1eb53649748b35f0bd5811119504
Lucas Hebmuller <108936216+LucasHebmuller@users.noreply.github.com>
2024-07-01 17:12:50 -0300

    aula 11

---
+++ exercicios/Condições/ex008.js
@@ -0,0 +1,6 @@
+var vel = 45
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+if (vel > 60)
+    console.log('Você está rápido demais!')
+console.log('Sempre utilize cinto de segurança.')
---
+++ exercicios/Condições/ex009.js
@@ -0,0 +1,7 @@
+var nacionalidade = 'Estados Unidos'
+console.log(`Você nasceu nesse país: ${nacionalidade}`)
+
+if(nacionalidade == 'Brasil')
+    console.log('Logo és brasileiro')
+else 
+    console.log('Logo és estrangeiro')
