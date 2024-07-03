9e380f84613f4b59ab3f775d0e92dd683c3a0ad4
Lucas Hebmuller <108936216+LucasHebmuller@users.noreply.github.com>
2024-07-03 19:39:47 -0300

    finish pratica 4

--- exercicios/Pratica 4/script.js
+++ exercicios/Pratica 4/script.js
@@ -9,19 +9,23 @@ function contagem() {
     else {
         res.innerHTML = '<p>Contando:</p>'
 
-        if (Number(passo.value) <= 0) {
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (p <= 0) {
             window.alert('Passo inválido! Considerando PASSO 1.')
-            passo = 1
+            p = 1
         }
 
-        if (Number(inicio.value) < Number(fim.value)) {
-            for(var i = Number(inicio.value); i <= Number(fim.value); i += Number(passo.value)) {
-                res.innerHTML += ` ${i}  \u{1F449}`
+        if (i < f) {
+            for(var j = i; j <= f; j += p) {
+                res.innerHTML += ` ${j}  \u{1F449}`
             }
         }
-        else if (Number(inicio.value) > Number(fim.value)) {
-            for(var i = Number(fim.value); i <= Number(inicio.value); i -= Number(passo.value)) {
-                res.innerHTML += ` ${i}  \u{1F449}`
+        else {
+            for(var j = i; j >= f; j -= p) {
+                res.innerHTML += ` ${j}  \u{1F449}`
             }
         }
         res.innerHTML += `\u{1F3C1}`
